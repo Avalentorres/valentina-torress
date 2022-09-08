7a0cbbc4fbdad236178ee0298cf4ce76c82cbbe3
Valentina <91225414+Avalentorres@users.noreply.github.com>
2022-09-08 16:02:26 -0300

    Create ItemCount.js

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./ItemCount.css"
+
+const ItemCount = () => {
+    const stock = 12;
+    let [contador, setContador] = useState(0);
+
+    const imprimirContador = () =>{
+        // eslint-disable-next-line no-template-curly-in-string
+        console.log('La cantidad seleccionada es: ${contador}');
+    }
+
+    if (contador === 0 ){
+        return(
+            <div className="contador-flex">
+                <button className="contador-btn btn-bloqueado"></button>
+                <h2>{contador}</h2>
+                <button className="contador-btn" onClick={() => setContador(++contador)}>+ </button>
+                <button onClick={imprimirContador}>Confirmar</button>
+
+            </div>
+
+        )
+    } else if (contador >= 1 && contador < stock){
+        return(
+            <div className="contador-flex">
+                <button className="contador-btn" onClick={() => setContador(--contador)}>-</button>
+                <h2>{contador}</h2>
+                <button className="contador-btn btn-bloqueado">+</button>
+                <button onClick={imprimirContador}>Confirmar</button>
+            </div>
+        )
+    }
+}
+
+export default ItemCount 
