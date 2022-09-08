a12c9dad1fe50e5e834f120d5a6b976a909298a2
Valentina <91225414+Avalentorres@users.noreply.github.com>
2022-09-08 16:52:19 -0300

    ItemListContainer.js

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,12 @@
+
+import React, {useState, useEffect} from "react";
+
+    const ItemListContainer = ({title}) =>{
+        return(
+        <h1 className="text-center">{title}</h1>
+    )
+}
+
+    
+
+export default ItemListContainer;
